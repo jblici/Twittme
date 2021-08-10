d43143bd763b62dd5e1b2e8aad20a3f68323145e
jblici <juambif@gmail.com>
2021-08-10 13:45:23 -0300

    bootstrap added, api rest working

--- tweets/views.py
+++ tweets/views.py
@@ -4,7 +4,24 @@ from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1> Hello World </h1>")
+    # return HttpResponse("<h1> Hello World </h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    '''
+    REST API VIEW
+    Consume by Javascript/Swift/Java/iOS/Android
+    return json data
+    '''
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     '''
