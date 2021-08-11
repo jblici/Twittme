a7eb328b8429998c7429dafd3cdaf9c50b9ec6cb
jblici <juambif@gmail.com>
2021-08-11 12:55:43 -0300

    serialize django model object and ajax added

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ah2z@p8*rhn8r^n@$smjn-xp!lf_ho=vpw)nvh#pi5jq-+nah6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
